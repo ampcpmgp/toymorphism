f1b57fe3ef7702da531009db4c5cdf5a1da8784d
ampcpmgp <ampcpmgp@gmail.com>
2021-09-22 23:44:52 +0900

    Button Style -> Base Style

---
+++ src/components/Core/Base.stories.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Base from "./Base.svelte";
+</script>
+
+<Meta
+  title="Core/Base"
+  component={Base}
+  parameters={{
+    backgrounds: {
+      default: "gray",
+    },
+  }}
+  argTypes={{
+    onClick: { action: "onClick" },
+  }}
+/>
+
+<Template let:args>
+  <Base {...args}>
+    <div class="box" />
+  </Base>
+</Template>
+
+<Story name="Normal" args={{}} />
+<Story name="Hidden false" args={{ hidden: false }} />
+
+<style>
+  .box {
+    width: 150px;
+    height: 40px;
+    background-color: white;
+  }
+</style>
---
+++ src/components/Core/Base.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let hidden = true;
+</script>
+
+<div class:hidden><slot /></div>
+
+<style>
+  div {
+    display: inline-grid;
+    place-items: center;
+    border-radius: 1rem;
+    background-color: #fc769f;
+
+    /* prettier-ignore */
+    box-shadow:
+      /* top */
+      0rem -0.1rem 0rem rgba(255, 255, 255, 0.7),
+      0rem -0.2rem 0rem #cb4966,
+
+      /* left & right */
+      0.08rem -0.1rem 0rem #cb4966,
+      -0.08rem -0.1rem 0rem #cb4966,
+      0.1rem 0rem 0rem #cb4966,
+      -0.1rem 0rem 0rem #cb4966,
+      0.08rem 0.2rem 0rem #cb4966,
+      -0.08rem 0.2rem 0rem #cb4966,
+      0.06rem 0.4rem 0rem #cb4966,
+      -0.06rem 0.4rem 0rem #cb4966,
+
+      /* bottom */
+      0rem 0.4rem 0rem #cb4966,
+      0rem 0.6rem 0.1rem #be4265,
+      0rem 1rem 0.4rem 0rem rgba(128, 128, 128, 1),
+
+      /* blank */
+      0 0 0 transparent
+      ;
+  }
+
+  .hidden {
+    overflow: hidden;
+  }
+</style>
