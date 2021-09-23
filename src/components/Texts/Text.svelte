b2dcc5a76d9d559584dd45e9b2b5c476b1226a1b
ampcpmgp <ampcpmgp@gmail.com>
2021-09-24 00:39:36 +0900

    Toy Text #1

---
+++ src/components/Texts/Text.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let size = "1rem";
+</script>
+
+<p style="--size: {size}">
+  <slot />
+</p>
+
+<style>
+  p {
+    display: inline-grid;
+    place-items: center;
+
+    margin: 0;
+    color: #fc769f;
+    font-size: var(--size);
+    line-height: calc(var(--size) * 1);
+
+    /* prettier-ignore */
+    text-shadow:
+      /* left & right */
+      calc(var(--size) * 0.02) 0rem 0rem #cb4966,
+      calc(var(--size) * -0.02) 0rem 0rem #cb4966,
+
+      calc(var(--size) * 0.02) calc(var(--size) * 0.1) 0rem #cb4966,
+      calc(var(--size) * -0.02) calc(var(--size) * 0.1) 0rem #cb4966,
+
+      /* bottom */
+      0rem calc(var(--size) * 0.05) 0rem #cb4966,
+      0rem calc(var(--size) * 0.1) 0rem #be4265,
+
+      /* blank */
+      0 0 0 transparent
+      ;
+  }
+</style>
