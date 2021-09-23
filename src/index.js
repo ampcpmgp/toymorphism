5ff950441939aeaeccb145deec0c021f829b8070
ampcpmgp <ampcpmgp@gmail.com>
2021-09-23 22:08:04 +0900

    fix error

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-export { default as Button } from "./components/Buttons.svelte";
+export { default as Button } from "./components/Buttons/Button.svelte";
+export { default as Base } from "./components/Core/Base.svelte";
+export { default as Frame } from "./components/Core/Frame.svelte";
