9c10bd63e98f56cbd55bfa6434808ef9e5ee8c94
Akira Hayatake <ampcpmgp@gmail.com>
2021-09-23 18:41:50 +0900

    Toy Frame #12 (#49)

---
+++ src/components/Core/Frame.stories.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Frame from "./Frame.svelte";
+</script>
+
+<Meta
+  title="Core/Frame"
+  component={Frame}
+  parameters={{
+    backgrounds: {
+      default: "gray",
+    },
+  }}
+  argTypes={{
+    onClick: { action: "onClick" },
+  }}
+/>
+
+<Template let:args>
+  <Frame {...args}>
+    <div class="box" />
+  </Frame>
+</Template>
+
+<Story name="Normal" args={{}} />
+
+<style>
+  .box {
+    width: 50vw;
+    height: 50vh;
+    background-color: white;
+  }
+</style>
---
+++ src/components/Core/Frame.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Base from "./Base.svelte";
+</script>
+
+<Base>
+  <div>
+    <slot />
+  </div>
+</Base>
+
+<style>
+  div {
+    margin: 0.25rem;
+    overflow: hidden;
+    border-radius: 1rem;
+  }
+</style>
