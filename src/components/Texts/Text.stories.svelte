79df271a4a37486fc585d33f8927ed2074a43b45
Akira Hayatake <ampcpmgp@gmail.com>
2021-09-25 12:39:53 +0900

    Toy Icon (Check if Toy Text is available) #53 (#78)

--- src/components/Texts/Text.stories.svelte
+++ src/components/Texts/Text.stories.svelte
@@ -17,7 +17,18 @@
 />
 
 <Template let:args>
-  <Text {...args}>{args.message}</Text>
+  {#if args.message}
+    <Text {...args}>{args.message}</Text>
+  {:else if args.icon}
+    <Text {...args}>
+      <div>
+        <i class="fas fa-paw" />
+        <i class="fas fa-hippo" />
+        <i class="fas fa-kiwi-bird" />
+        <i class="fas fa-horse" />
+      </div>
+    </Text>
+  {/if}
 </Template>
 
 <Story
@@ -32,5 +43,13 @@
   args={{
     size: "5rem",
     message: "1`˜!@#-$~%あநூந்सैसមួᏗᏍ백叢张夕張",
+  }}
+/>
+
+<Story
+  name="Font awesome"
+  args={{
+    size: "5rem",
+    icon: true,
   }}
 />
