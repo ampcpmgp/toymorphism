e256a054e66c0fbf9e2618832ecb4ecbe4245b6d
Akira Hayatake <ampcpmgp@gmail.com>
2021-09-27 00:32:13 +0900

    Toy Link (#89)

---
+++ src/components/Links/Link.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Text from "./../Texts/Text.svelte";
+
+  export let size = "1rem";
+  export let color;
+
+  /** see: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#attributes */
+  export let attrs = {};
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<a {...attrs} style="--color: {color};">
+  <Text {size} {color}><slot /></Text>
+</a>
+
+<style>
+  a {
+    display: inline-flex;
+    text-decoration-color: var(--color);
+  }
+</style>
