15d54240bd324d3f18ea43d0bf53d7edf845822a
Akira Hayatake <ampcpmgp@gmail.com>
2021-09-30 22:23:00 +0900

    Toy Code (#106)

---
+++ src/components/Texts/Code.stories.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Code from "./Code.svelte";
+</script>
+
+<Meta
+  title="Texts/Code"
+  component={Code}
+  parameters={{
+    backgrounds: {
+      default: "white",
+    },
+  }}
+  argTypes={{
+    baseColor: {
+      control: {
+        type: "color",
+      },
+    },
+    textColor: {
+      control: {
+        type: "color",
+      },
+    },
+    onClick: { action: "onClick" },
+  }}
+/>
+
+<Template let:args>
+  <Code {...args}>{args.message}</Code>
+</Template>
+
+<Story
+  name="Hello world"
+  args={{
+    code: `window.Prism = window.Prism || {};
+window.Prism.manual = true;`,
+    baseColor: "white",
+    textColor: "black",
+    attrs: {},
+  }}
+/>
---
+++ src/components/Texts/Code.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { colors } from "../../stores/theme";
+
+  import Frame from "../Core/Frame.svelte";
+
+  export let code = "";
+  export let baseColor = $colors.base;
+  export let textColor = $colors.text;
+
+  /** see: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/div#attributes */
+  export let attrs = {};
+</script>
+
+<Frame color={baseColor}>
+  <pre
+    class="wrapper">
+    <code {...attrs} style="{attrs.style}; --text-color: {textColor};">
+      {code}
+    </code>
+  </pre>
+</Frame>
+
+<style>
+  .wrapper {
+    white-space: pre-wrap;
+    margin: 10px;
+    font-size: 14px;
+  }
+
+  code {
+    color: var(--text-color);
+  }
+</style>
