b1a29c0c9587c6babcbf1664f167240b41b15e59
ampcpmgp <ampcpmgp@gmail.com>
2021-10-08 21:11:24 +0900

    svelte-spa-router 導入

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,48 +1,6 @@
 <script>
-  import { Button, Text } from "toymorphism";
-
-  import logo from "./assets/svelte.png";
+  import Router from "svelte-spa-router";
+  import routes from "./routes";
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Button baseColor="#fc769f">
-    <Text attrs={{ style: "padding: 20px;" }}>Click me!</Text>
-  </Button>
-</main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-  }
-</style>
+<Router {routes} />
