29040a09efd74462df06edb4079e3b693cb65049
ampcpmgp <ampcpmgp@gmail.com>
2021-10-16 12:24:47 +0900

    add template component

---
+++ packages/svelte/components/_!Templates/COMPONENT_NAME.stories.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import COMPONENT_NAME from "./COMPONENT_NAME.svelte";
+</script>
+
+<Meta
+  title="_Templates/COMPONENT_NAME"
+  component={COMPONENT_NAME}
+  parameters={{
+    backgrounds: {
+      default: "gray",
+    },
+  }}
+  argTypes={{
+    onClick: { action: "onClick" },
+  }}
+/>
+
+<Template let:args>
+  <COMPONENT_NAME {...args} on:click={args.onClick}>child</COMPONENT_NAME>
+</Template>
+
+<Story name="No settings" args={{}} />
+
+<Story
+  name="Default"
+  args={{
+    custom: "custom",
+  }}
+/>
---
+++ packages/svelte/components/_!Templates/COMPONENT_NAME.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<div>a</div>
+
+<style>
+</style>
